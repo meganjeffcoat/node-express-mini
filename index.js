dc8e23d1e4cfc2912b2ea3565dd26b436cb46230
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 15:14:30 -0600

    installed express and set up initial endpoint ensuring server works

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+server.listen(4000, () => {
+    console.log('\n *** Running on port 4000 *** \n');
+});
