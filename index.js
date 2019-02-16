609f68cbdded378949ff2864fe45ed56fc202458
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 15:59:48 -0600

    basic set up of all endpoints created

--- index.js
+++ index.js
@@ -23,15 +23,56 @@ server.get('/api/users', (req, res) => {
         })
 });
 
-server.post('api/users', (req, res) => {
+server.post('/api/users', (req, res) => {
     const user = req.body;
     db  
         .insert(user)
         .then(user => {
             res.status(201).json({ success: true, user });
         })
+        .catch(err => {
+            res.status(err.code).json({ success: false, message: err.message})
+        })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db  
+        .findById(id)
+        .then(user => {
+            res.status(200).json({ success: true, user});
+        })
+        .catch(err => {
+            res.status(err.code).json({ success: false, message: err.message})
+        })
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db
+        .remove(userId)
+        .then(deleteUser => {
+            res.status(204).end();
+        })
         .catch(({ code, message }) => {
             res.status(code).json({ success: false, message})
+        });
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db
+        .update(id, changes)
+        .then(updatedUser => {
+            if (updatedUser) {
+                res.status(200).json({ success: true, updatedUser });
+            } else {
+                res.status(404).json({ success: false, message: 'This user does not exist'})
+            }
+        })
+        .catch(({ code, message }) => {
+            res.status(code).json({ success: false, message })
         })
 });
 
