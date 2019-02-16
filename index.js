56430e9cf5f499cc1cabc819eaa19d25f5682311
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 15:26:57 -0600

    GET for users endpoint works

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const server = express();
 
 server.use(express.json());
 
-
+//Initial endpoint to test that it is working
 server.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+//Project endpoints
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json({ success: true, users });
+        })
+        .catch(err => {
+            res.status(err.code).json({ success: false, message: err.message });
+        })
+});
+
+
 
 server.listen(4000, () => {
     console.log('\n *** Running on port 4000 *** \n');
